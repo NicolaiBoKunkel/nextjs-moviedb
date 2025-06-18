d71bf7985c1f65382819443840967a9b543bd0e9
Nicolai Bo Kunkel <nicolai@kunkel.dk>
2025-06-18 11:06:49 +0200

    rollback timeouts

--- cypress/e2e/mediaDetail.cy.ts
+++ cypress/e2e/mediaDetail.cy.ts
@@ -4,38 +4,38 @@ describe('Media Cast Navigation Flow', () => {
   });
 
   it('searches for Star Wars and visits Mark Hamill’s actor page', () => {
-    cy.get('input[placeholder="Search for Movies or TV Shows..."]', { timeout: 10000 })
+    cy.get('input[placeholder="Search for Movies or TV Shows..."]',)
       .scrollIntoView()
       .type('Star Wars');
 
-    cy.contains('Star Wars', { timeout: 15000 }).should('be.visible').click();
+    cy.contains('Star Wars',).should('be.visible').click();
 
-    cy.url({ timeout: 10000 }).should('include', '/movie/');
-    cy.contains('Star Wars', { timeout: 10000 }).should('exist');
+    cy.url().should('include', '/movie/');
+    cy.contains('Star Wars',).should('exist');
     cy.contains('Cast').scrollIntoView();
 
-    cy.contains('Mark Hamill', { timeout: 10000 }).should('be.visible').click();
-    cy.url({ timeout: 10000 }).should('include', '/person/');
-    cy.contains('Mark Hamill', { timeout: 10000 }).should('exist');
-    cy.contains('Biography', { timeout: 10000 }).should('exist');
-    cy.contains('Known For', { timeout: 10000 }).should('exist');
+    cy.contains('Mark Hamill',).should('be.visible').click();
+    cy.url().should('include', '/person/');
+    cy.contains('Mark Hamill',).should('exist');
+    cy.contains('Biography',).should('exist');
+    cy.contains('Known For',).should('exist');
   });
 
   it('searches for Breaking Bad and visits Bryan Cranston’s actor page', () => {
-    cy.get('input[placeholder="Search for Movies or TV Shows..."]', { timeout: 10000 })
+    cy.get('input[placeholder="Search for Movies or TV Shows..."]',)
       .scrollIntoView()
       .type('Breaking Bad');
 
-    cy.contains('Breaking Bad', { timeout: 15000 }).should('be.visible').click();
+    cy.contains('Breaking Bad',).should('be.visible').click();
 
-    cy.url({ timeout: 10000 }).should('include', '/tv/');
-    cy.contains('Breaking Bad', { timeout: 10000 }).should('exist');
+    cy.url().should('include', '/tv/');
+    cy.contains('Breaking Bad',).should('exist');
     cy.contains('Cast').scrollIntoView();
 
-    cy.contains('Bryan Cranston', { timeout: 10000 }).should('be.visible').click();
-    cy.url({ timeout: 10000 }).should('include', '/person/');
-    cy.contains('Bryan Cranston', { timeout: 10000 }).should('exist');
-    cy.contains('Biography', { timeout: 10000 }).should('exist');
-    cy.contains('Known For', { timeout: 10000 }).should('exist');
+    cy.contains('Bryan Cranston',).should('be.visible').click();
+    cy.url().should('include', '/person/');
+    cy.contains('Bryan Cranston',).should('exist');
+    cy.contains('Biography',).should('exist');
+    cy.contains('Known For',).should('exist');
   });
 });
